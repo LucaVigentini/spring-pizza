db4d312c8b7ea97bf0f1af77ad6b8954147814ef
LucaVigentini <luca.vigentini97@gmail.com>
2022-01-10 18:19:31 +0100

    first commit

---
+++ src/main/java/org/generation/italy/model/Pizza.java
@@ -0,0 +1,76 @@
+package org.generation.italy.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+public class Pizza {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NonNull
+	private String nome;
+	
+	@NonNull
+	private String descrizione;
+	
+	@NonNull 
+	private double prezzo;
+	
+	
+	@ManyToMany
+	private List<Ingrediente> ingredienti;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public List<Ingrediente> getIngredienti() {
+		return ingredienti;
+	}
+
+	public void setIngredienti(List<Ingrediente> ingredienti) {
+		this.ingredienti = ingredienti;
+	}
+	
+
+
+}
